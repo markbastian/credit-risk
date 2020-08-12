42ce574bfc1c1ce0993ac67636d2ee7dec1dff60
Mark Bastian <mbastian@clearwateranalytics.com>
2020-08-11 23:56:14 -0600

    Need to add keras sequential layer as next try

--- models/baselinelgb.py
+++ models/baselinelgb.py
@@ -7,21 +7,19 @@ import numpy as np
 import pandas as pd
 
 
-def model(features, test_features, encoding='ohe', n_folds=5):
+def model(training_data, testing_data, n_folds=5):
     """Train and test a light gradient boosting model using
     cross validation.
 
     Parameters
     --------
-        features (pd.DataFrame):
+        training_data (pd.DataFrame):
             dataframe of training features to use
             for training a model. Must include the TARGET column.
-        test_features (pd.DataFrame):
+        testing_data (pd.DataFrame):
             dataframe of testing features to use
             for making predictions with the model.
-        encoding (str, default = 'ohe'):
-            method for encoding categorical variables. Either 'ohe' for one-hot encoding or 'le' for integer label encoding
-            n_folds (int, default = 5): number of folds to use for cross validation
+        n_folds (int, default = 5): number of folds to use for cross validation
 
     Return
     --------
@@ -32,63 +30,32 @@ def model(features, test_features, encoding='ohe', n_folds=5):
             dataframe with the feature importances from the model.
         valid_metrics (pd.DataFrame):
             dataframe with training and validation metrics (ROC AUC) for each fold and overall.
-
     """
 
-    # Extract the ids
-    train_ids = features['SK_ID_CURR']
-    test_ids = test_features['SK_ID_CURR']
-
     # Extract the labels for training
-    labels = features['TARGET']
+    y_train = training_data['TARGET']
 
-    # Remove the ids and target
-    features = features.drop(columns=['SK_ID_CURR', 'TARGET'])
-    test_features = test_features.drop(columns=['SK_ID_CURR'])
+    # Remove the feature_labels and target
+    X_train = training_data.drop(columns=['SK_ID_CURR', 'TARGET'])
+    test_ids = testing_data['SK_ID_CURR']
+    X_test = testing_data.drop(columns=['SK_ID_CURR'])
 
-    # One Hot Encoding
-    if encoding == 'ohe':
-        features = pd.get_dummies(features)
-        test_features = pd.get_dummies(test_features)
+    # One hot encode features
+    X_train = pd.get_dummies(X_train)
+    X_test = pd.get_dummies(X_test)
 
-        # Align the dataframes by the columns
-        features, test_features = features.align(test_features, join='inner', axis=1)
+    # Align the dataframes by the columns
+    X_train, X_test = X_train.align(X_test, join='inner', axis=1)
 
-        # No categorical indices to record
-        cat_indices = 'auto'
-
-    # Integer label encoding
-    elif encoding == 'le':
-
-        # Create a label encoder
-        label_encoder = LabelEncoder()
-
-        # List for storing categorical indices
-        cat_indices = []
-
-        # Iterate through each column
-        for i, col in enumerate(features):
-            if features[col].dtype == 'object':
-                # Map the categorical features to integers
-                features[col] = label_encoder.fit_transform(np.array(features[col].astype(str)).reshape((-1,)))
-                test_features[col] = label_encoder.transform(np.array(test_features[col].astype(str)).reshape((-1,)))
-
-                # Record the categorical indices
-                cat_indices.append(i)
-
-    # Catch error if label encoding scheme is not valid
-    else:
-        raise ValueError("Encoding must be either 'ohe' or 'le'")
-
-    print('Training Data Shape: ', features.shape)
-    print('Testing Data Shape: ', test_features.shape)
+    print('Training Data Shape: ', X_train.shape)
+    print('Testing Data Shape: ', X_test.shape)
 
     # Extract feature names
-    feature_names = list(features.columns)
+    feature_names = list(X_train.columns)
 
     # Convert to np arrays
-    features = np.array(features)
-    test_features = np.array(test_features)
+    X_train = np.array(X_train)
+    X_test = np.array(X_test)
 
     # Create the kfold object
     k_fold = KFold(n_splits=n_folds, shuffle=True, random_state=50)
@@ -97,21 +64,21 @@ def model(features, test_features, encoding='ohe', n_folds=5):
     feature_importance_values = np.zeros(len(feature_names))
 
     # Empty array for test predictions
-    test_predictions = np.zeros(test_features.shape[0])
+    test_predictions = np.zeros(X_test.shape[0])
 
     # Empty array for out of fold validation predictions
-    out_of_fold = np.zeros(features.shape[0])
+    out_of_fold = np.zeros(X_train.shape[0])
 
     # Lists for recording validation and training scores
     valid_scores = []
     train_scores = []
 
     # Iterate through each fold
-    for train_indices, valid_indices in k_fold.split(features):
+    for train_indices, valid_indices in k_fold.split(X_train):
         # Training data for the fold
-        train_features, train_labels = features[train_indices], labels[train_indices]
+        train_features, train_labels = X_train[train_indices], y_train[train_indices]
         # Validation data for the fold
-        valid_features, valid_labels = features[valid_indices], labels[valid_indices]
+        valid_features, valid_labels = X_train[valid_indices], y_train[valid_indices]
 
         # Create the model
         the_model = lgb.LGBMClassifier(n_estimators=10000, objective='binary',
@@ -122,7 +89,7 @@ def model(features, test_features, encoding='ohe', n_folds=5):
         # Train the model
         the_model.fit(train_features, train_labels, eval_metric='auc',
                       eval_set=[(valid_features, valid_labels), (train_features, train_labels)],
-                      eval_names=['valid', 'train'], categorical_feature=cat_indices,
+                      eval_names=['valid', 'train'], categorical_feature='auto',
                       early_stopping_rounds=100, verbose=200)
 
         # Record the best iteration
@@ -132,7 +99,7 @@ def model(features, test_features, encoding='ohe', n_folds=5):
         feature_importance_values += the_model.feature_importances_ / k_fold.n_splits
 
         # Make predictions
-        test_predictions += the_model.predict_proba(test_features, num_iteration=best_iteration)[:, 1] / k_fold.n_splits
+        test_predictions += the_model.predict_proba(X_test, num_iteration=best_iteration)[:, 1] / k_fold.n_splits
 
         # Record the out of fold predictions
         out_of_fold[valid_indices] = the_model.predict_proba(valid_features, num_iteration=best_iteration)[:, 1]
@@ -156,7 +123,7 @@ def model(features, test_features, encoding='ohe', n_folds=5):
     feature_importances = pd.DataFrame({'feature': feature_names, 'importance': feature_importance_values})
 
     # Overall validation score
-    valid_auc = roc_auc_score(labels, out_of_fold)
+    valid_auc = roc_auc_score(y_train, out_of_fold)
 
     # Add the overall scores to the metrics
     valid_scores.append(valid_auc)
